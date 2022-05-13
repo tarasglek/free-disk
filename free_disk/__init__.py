c4fdb9caeac3e9049cbb619feaff0123b8724749
Taras Glek <taras@glek.net>
2022-05-13 13:23:13 +0200

    dry-run

--- free_disk/__init__.py
+++ free_disk/__init__.py
@@ -29,7 +29,7 @@ def pretty(number):
     abs_number = abs(number)
     for unit,val in reversed(_DATA_SIZE_UNIT_BYTE_CONVERSION_FACTOR.items()):
         if abs_number > val:
-            return f'{round(number*1.00/val,2)}{unit}'
+            return f'{round(number/val,2)}{unit}'
     return str(number)
 
 def _data_size_to_bytes(size_with_unit: str) -> int:
